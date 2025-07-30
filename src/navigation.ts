85b054c5339dc8e8933e66cfb73f1c39631a8884
benstaf <mostafacebook@gmail.com>
2025-07-30 16:09:14 +0200

    Update navigation.ts

--- src/navigation.ts
+++ src/navigation.ts
@@ -20,13 +20,10 @@ export const footerData = {
     { text: 'Privacy Policy', href: getPermalink('/privacy') },
   ],
   socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
-    { ariaLabel: 'Telegram', icon: 'tabler:brand-telegram', href: '#' },
-    { ariaLabel: 'Discord', icon: 'tabler:brand-discord', href: '#' },
-    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
+    { ariaLabel: 'Telegram', icon: 'tabler:brand-telegram', href: 'https://t.me/jiangstreet' },
+    { ariaLabel: 'Discord', icon: 'tabler:brand-discord', href: 'https://forms.gle/zeva2efiZ2oSj6SG7' },
   ],
   footNote: `
-    &copy; 2025 Jiang Street. All rights reserved. | Research-driven Quantitative Trading Organization.
+    &copy; 2025 Jiang Street. All rights reserved.
   `,
 };
